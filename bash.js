0752a66021fbc0b9dbe03150072193466edfa6a0
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-03-05 12:45:25 -0500

    got pwd working

---
+++ bash.js
@@ -0,0 +1,14 @@
+// output a prompt
+process.stdout.write('prompt > ');
+
+//the stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim()
+    if (cmd === 'pwd'){
+        process.stdout.write(process.cwd())
+        process.stdout.write('\nprompt > ')
+    }else{
+        process.stdout.write('You typed: ' + cmd)
+        process.stdout.write('\nprompt > ')
+    }
+})
